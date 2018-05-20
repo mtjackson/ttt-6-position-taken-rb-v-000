62b661d04cafa97e2c34424b1afd16bb70c5387c
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-20 03:21:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,12 @@
 
 def position_taken?(board, index)
   if board[index] == " "
-
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  elsif board[index] == "X" || "O"
+    return true
+  end
 end
